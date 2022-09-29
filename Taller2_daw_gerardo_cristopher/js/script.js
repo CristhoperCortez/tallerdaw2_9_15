04826108dc23c40ec2fc862c8030c0a840a53880
CristhoperCortez <104181323+CristhoperCortez@users.noreply.github.com>
2022-09-29 15:09:51 -0600

    Update script.js

--- Taller2_daw_gerardo_cristopher/js/script.js
+++ Taller2_daw_gerardo_cristopher/js/script.js
@@ -4,4 +4,13 @@ pedido.comboperso = 5.75;
 pedido.comboinfantil= 3.50;
 pedido.cantcombo=1;
 
-pedido.ensalada = 1.50
+pedido.ensalada = 1.50;
+pedido.papas = 1.25;
+pedido.pgrande = 1.75;
+pedido.pmedia = 1.50;
+pedido.ppequeña = 1.25;
+pedido.bgrande = 1.50;
+pedido.bmedia = 1.25;
+pedido.bpequeña = 1;
+pedido.cafe = 0.50;
+pedido.postre = 1.25;
